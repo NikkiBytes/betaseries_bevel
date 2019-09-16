5849846d56b3f4d16a7a5d0acd81911af65c974f
Jenny Gilbert <jennyrgilb@gmail.com>
2019-09-16 14:27:00 -0400

    adding r script

--- 5_analysis/correlation_matrix.R
+++ 5_analysis/correlation_matrix.R
@@ -1,13 +1,27 @@
-install.packages("Hmisc")
-library("Hmisc")
-install.packages("corrplot")
+library(Hmisc)
 library(corrplot)
 
-setwd('/Users/jennygilbert/Documents/betaseries_bevel/5_analysis/FSLNets_test/reward/')
+setwd('/Users/jennygilbert/Documents/betaseries_bevel/4_combine_timeseries/sync_9_16/reward/')
 
-mydata = read.csv("sub-001_reward.txt", header = FALSE, sep = '\t')
+mydata = read.csv("sub-003_reward.txt", header = FALSE, sep = '\t')
+names(mydata) <- c("Amygdala_L", "Amygdala_R", "Dorsal_striatum_L", "Dorsal_striatum_R", "Fusiform_gyrus_L", "Fusiform_gyrus_R", "Hippocampus_L", "Hippocampus_R", "Insula_L",
+                 "Insula_R", "Intracalcarine_cortex_L", "Intracalcarine_cortex_R", "lOFC_R", "mOFC_L", "mOFC_R", "Oral_somatosensory_cortex_L", "Oral_somatosensory_cortex_R", "Precuneus_L", 
+                 "Precuneus_R", "Ventral_striatum_L", "Ventral_striatum_R", "vlPFC_L", "vlPFC_R", "vlThalamus_L" , "vlThalamus_R", "vmPFC_L", "vmPFC_R")
+
 mydata.rcorr = rcorr(as.matrix(mydata))
+print(mydata.rcorr)
+
 mydata.coeff = mydata.rcorr$r
+print(mydata.coeff)
+write.table(mydata.coeff, file = "sub-003_reward_corrcoeff.csv", append = FALSE, sep = ",", dec = ".",
+            row.names = TRUE, col.names = TRUE)
+
 mydata.p = mydata.rcorr$P
-corrplot(mydata.cor)
-heatmap(x = mydata.cor, symm = TRUE)
+print(mydata.p) 
+write.table(mydata.p, file = "sub-003_reward_pvalue.csv", append = FALSE, sep = ",", dec = ".",
+            row.names = TRUE, col.names = TRUE)
+
+pdf("sub-003_reward_rplot.pdf")
+corrplot(mydata.rcorr$r, type="upper", order="hclust", 
+         p.mat = mydata.rcorr$P, sig.level = 0.01, insig = "blank")
+dev.off() 
